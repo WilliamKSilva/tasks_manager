71458546e994d57a05aac41f92690f3abe0e38e3
William <wilz.kelvin12@gmail.com>
2022-07-20 14:24:00 -0300

    feat: add cors and fix cors problems

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors(corsOptions));
 
 export { app } 
